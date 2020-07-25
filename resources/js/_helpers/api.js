4287191284384707d46a2cf87ccd817dfdef52ea
Safal Pokharel <pokharelsafal66@gmail.com>
2020-07-26 08:24:32 +1000

    Update api.js

--- resources/js/_helpers/api.js
+++ resources/js/_helpers/api.js
@@ -1,8 +1,8 @@
 import axios  from 'axios';
 
 let baseApi = axios.create({
-    // baseURL: "http://laravel-react-redux.herokuapp.com/api"
-    baseURL: "http://localhost:8000/api"
+    baseURL: "http://laravel-react-redux.herokuapp.com/api"
+    // baseURL: "http://localhost:8000/api"
 })
 
 let api = () => {
