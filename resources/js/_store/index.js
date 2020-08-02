283a3b3a869b51ac86323b06edc003bdddb4cdeb
Safal Pokharel <safalpokharel@Safals-MacBook-Pro.local>
2020-08-02 22:20:30 +1000

    changes in store and exclue dev tool in production

--- resources/js/_store/index.js
+++ resources/js/_store/index.js
@@ -6,12 +6,19 @@ const initialState = {};
 
 const middleware = [ thunk ];
 
+// eslint-disable-next-line no-underscore-dangle
+let devTools = window.__REDUX_DEVTOOLS_EXTENSION__ &&
+    window.__REDUX_DEVTOOLS_EXTENSION__();
+if (process.env.NODE_ENV === 'prod' || process.env.NODE_ENV === 'production') {
+    devTools = a => a;
+}
+
 const store = createStore(
     rootReducer,
     initialState,
     compose(
         applyMiddleware(...middleware),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+        devTools,
     )
 
 );
