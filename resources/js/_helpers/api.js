6348070a4dabe03821bb45d9310f5d4671c867a3
Safal Pokharel <safalpokharel@Safals-MacBook-Pro.local>
2020-09-23 18:59:42 +1000

    change in url

--- resources/js/_helpers/api.js
+++ resources/js/_helpers/api.js
@@ -3,7 +3,7 @@ import axios  from 'axios';
 let baseApi = axios.create({
     baseURL: process.env.NODE_ENV === 'development'
         ? "http://localhost:8000/api"
-        : "http://laravel-react-redux.herokuapp.com/api",
+        : "https://laravel-react-redux.herokuapp.com/api",
 })
 
 let api = () => {
